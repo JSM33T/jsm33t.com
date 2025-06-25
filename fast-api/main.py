1d71d24e278e7f9a500a5c28aaa9f39532a56ce4
jsm33t <jasmeetsinghkainth@gmail.com>
2025-06-26 02:57:55 +0530

    save config
    fast api deployment
    
    updates
    
    update scripts
    
    check deployment for fast api
    
    update fast api and deployment check
    
    Update deploy-fastapi.yml
    
    update v3
    
    v4
    
    v5 updates
    
    v6
    
    update script
    
    v7 changes
    
    v4
    
    v5 updates
    
    v6
    
    update script
    
    v7 changes

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Under Rennovation"}
+    # Access the environment variable "ENVX"
+    envx_value = os.getenv("ENVX", "Not Set")
+
+    return {"message": "Under Renovation type shi v7.", "envx": envx_value}
