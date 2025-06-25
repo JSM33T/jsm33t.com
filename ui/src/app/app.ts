6004ecddbffc9d3a18f1a624338738f5c370b0ff
jsm33t <jasmeetsinghkainth@gmail.com>
2025-06-26 00:23:30 +0530

    :art: css overrides and navbar

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, effect, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from "./shared/navbar/navbar";
+import { Navbar } from "./components/shared/navbar/navbar";
 import { NetworkService } from './services/network-service';
 import { CommonModule, NgIf } from '@angular/common';
 import { Chatbot } from './components/shared/chatbot/chatbot';
